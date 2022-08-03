f14fe3598bd2c0871320af6466d5a9c6eeb60d8b
Owolabi Oluwarotimi <ffftimi@gmail.com>
2022-08-03 20:01:09 +0100

    undeclared variable

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 import click
 
-mae;
 @click.command(help="This is just a hello app. It does nothing.")
 @click.option("--name", prompt="I need your name", help="Need name")
 @click.option("--color", prompt="I need your color", help="This is your color")
